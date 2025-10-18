6636351efb6de984c2aef5abf07203c59bce9fb1
Lucas Loreggia <lucas@lloreggia.ch>
2025-10-18 19:04:47 +0200

    Additional backup containers, added script to update creative from survival

---
+++ update-creative.ps1
@@ -0,0 +1,27 @@
+$execCompose = "docker compose -p minecraft"
+
+Write-Output "Backing up mc-survival";
+iex "$execCompose exec mc-survival-backup backup now"
+Write-Output "Backing up mc-creative";
+iex "$execCompose exec mc-creative-backup backup now"
+
+Write-Output "Stopping mc-creative";
+iex "$execCompose stop mc-creative"
+
+Write-Output "Removing mc-creative data";
+Remove-Item ./data/creative/* -Recurse -Force -ErrorAction SilentlyContinue
+
+Write-Output "Creating mc-survival-to-creative-restore";
+iex "$execCompose create mc-survival-to-creative-restore --scale mc-survival-to-creative-restore=1"
+
+Write-Output "Starting mc-survival-to-creative-restore";
+iex "$execCompose start mc-survival-to-creative-restore"
+
+Write-Output "Waiting for mc-survival-to-creative-restore";
+iex "$execCompose wait mc-survival-to-creative-restore"
+
+Write-Output "Removing mc-survival-to-creative-restore";
+iex "$execCompose create --scale mc-survival-to-creative-restore=0"
+
+Write-Output "Starting mc-creative";
+iex "$execCompose start mc-creative"
